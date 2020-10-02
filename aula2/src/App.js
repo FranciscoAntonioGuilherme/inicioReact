ab16394fef0e5458577e495a50c85dd775641723
FranciscoAntonioGuilherme <70604374+FranciscoAntonioGuilherme@users.noreply.github.com>
2020-10-02 18:40:19 -0300

    02.10.2020

---
+++ aula2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './componet/Button';
+
+function App() {
+  return (
+    <div>
+        <Button title='teste'>
+            Salvar
+        </Button>
+    </div>
+  );
+}
+
+export default App;
